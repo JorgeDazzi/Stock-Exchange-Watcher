04d0d9c66485e49d41b886f6159b7b946a9ad0bf
Dazzi <jorge.dazzi@gmail.com>
2017-11-24 00:03:38 -0200

    Version 1.0 done

--- __main__.py
+++ __main__.py
@@ -1,6 +1,9 @@
 from maleficentBrain import MaleficentBrain
-
 import sys
+
+"""
+@Pizza
+"""
 
 stockList = ['GOLL4.SA', 'AZUL4.SA', 'CCRO3.SA','BBAS3.SA', 'BBDC4.SA', 'WEGE3.SA', 'EMBR3.SA', 'BRFS3.SA' , 'BBSE3.SA']
 
